11b20ec46e9eb8842cba4b85ce016f54b0ea2f5b
IanGonzalezTDF <102770923+IanGonzalezTDF@users.noreply.github.com>
2022-04-29 13:43:47 -0300

    Fin

--- carpetadatos/datos.ts
+++ carpetadatos/datos.ts
@@ -1,9 +1,9 @@
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.getElementById("dato");
 let rotulo = document.getElementById("rotulo");
+rotulo.innerHTML = "Ingrese un dato";
 btnEnv.addEventListener("click", () => {
   //TU CODIGO VA AQUI, POR EJEMPLO:
   console.log("el dato ingresado es", dato.value);
   //
 });
-rotulo.innerHTML = "Ingrese un dato";
